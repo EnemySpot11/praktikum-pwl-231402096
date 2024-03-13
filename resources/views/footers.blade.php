3f0a25ec1c58bf47ae0818de6fa0d767d3af1d13
Alfathin Suwailim <alfasuwu596@gmail.com>
2024-03-13 10:41:59 +0700

    tugas 2

---
+++ resources/views/footers.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.layout')
+
+@section('thefooter')
+<p>aku anak indigo</p>
+  @endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+// Route::get('/', function () {
+//     return view('footer');
+// });
+
+// Route::get('/', function () {
+//     return view('landing');
+// });
+
+
+//  Route::get('/', function () {
+//      return view('articles');
+//  });
+
+//  Route::get('/aerticles?{article}', function ($slug) {
+//     $path = _DIR_ . "/resources/articles/($slug).php";
+
+//     $article = file_get_contents($path);
+
+//     return view('article',[
+//         'aarticle'=>$article
+//     ]);
+// });
